2c1001fea0f33821e6b408c78dd18d3704af7fd5
AllenXin <33273090+NingAllenXin@users.noreply.github.com>
2018-03-20 16:03:30 -0500

    Create ROR4.java

---
+++ pitest/pitest/src/main/java/org/pitest/mutationtest/engine/gregor/Mymutators/ROR4.java
@@ -0,0 +1,81 @@
+package org.pitest.mutationtest.engine.gregor.Mymutators;
+
+import org.objectweb.asm.MethodVisitor; 
+import org.objectweb.asm.Opcodes;
+import org.pitest.mutationtest.engine.gregor.AbstractJumpMutator;
+import org.pitest.mutationtest.engine.gregor.MethodInfo;
+import org.pitest.mutationtest.engine.gregor.MethodMutatorFactory;
+import org.pitest.mutationtest.engine.gregor.MutationContext;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum ROR4 implements MethodMutatorFactory {
+
+  ROR_MUTATOR4;
+
+  @Override
+  public MethodVisitor create(final MutationContext context,
+                              final MethodInfo methodInfo, final MethodVisitor methodVisitor) {
+    return new RORMethodVisitor(this, context, methodVisitor);
+  }
+
+  @Override
+  public String getGloballyUniqueId() {
+    return this.getClass().getName();
+  }
+
+  @Override
+  public String getName() {
+    return name();
+  }
+
+}
+
+class RORMethodVisitor extends AbstractJumpMutator {
+
+  private static final String                     DESCRIPTION = "Relational operator replacement mutation (ROR)";
+  private static final Map<Integer, Substitution> MUTATIONS   = new HashMap<Integer, Substitution>();
+
+  static {
+    // Compares a single value to 0
+    
+    MUTATIONS.put(Opcodes.IFEQ, new Substitution(Opcodes.IFGT, DESCRIPTION));
+   
+    MUTATIONS.put(Opcodes.IFNE, new Substitution(Opcodes.IFGT, DESCRIPTION));
+    
+    MUTATIONS.put(Opcodes.IFLE, new Substitution(Opcodes.IFGT, DESCRIPTION));
+    
+    MUTATIONS.put(Opcodes.IFGE, new Substitution(Opcodes.IFGT, DESCRIPTION));
+   
+    MUTATIONS.put(Opcodes.IFGT, new Substitution(Opcodes.IFGE, DESCRIPTION));
+   
+    MUTATIONS.put(Opcodes.IFLT, new Substitution(Opcodes.IFGE, DESCRIPTION));
+
+
+    // Compares two values
+    
+    MUTATIONS.put(Opcodes.IF_ICMPNE, new Substitution(Opcodes.IF_ICMPGT, DESCRIPTION));
+   
+    MUTATIONS.put(Opcodes.IF_ICMPEQ, new Substitution(Opcodes.IF_ICMPGT, DESCRIPTION));
+    
+    MUTATIONS.put(Opcodes.IF_ICMPLE, new Substitution(Opcodes.IF_ICMPGT, DESCRIPTION));
+    
+    MUTATIONS.put(Opcodes.IF_ICMPGE, new Substitution(Opcodes.IF_ICMPGT, DESCRIPTION));
+    
+    MUTATIONS.put(Opcodes.IF_ICMPGT, new Substitution(Opcodes.IF_ICMPGE, DESCRIPTION));
+    
+    MUTATIONS.put(Opcodes.IF_ICMPLT, new Substitution(Opcodes.IF_ICMPGE, DESCRIPTION));
+  }
+
+  RORMethodVisitor(final MethodMutatorFactory factory,
+                   final MutationContext context, final MethodVisitor delegateMethodVisitor) {
+    super(factory, context, delegateMethodVisitor);
+  }
+
+  @Override
+  protected Map<Integer, Substitution> getMutations() {
+    return MUTATIONS;
+  }
+
+}
