25b2b307ba9c2ad912ce001d83d1589e63e0d60e
AllenXin <33273090+NingAllenXin@users.noreply.github.com>
2018-04-16 12:09:00 -0500

    Create OBBN.java

---
+++ pitest/pitest/src/main/java/org/pitest/mutationtest/engine/gregor/Mymutators/OBBN.java
@@ -0,0 +1,61 @@
+package org.pitest.mutationtest.engine.gregor.Mymutators;
+
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import org.pitest.mutationtest.engine.gregor.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum OBBN implements MethodMutatorFactory {
+
+  OBBN;
+
+  @Override
+  public MethodVisitor create(final MutationContext context,
+                              final MethodInfo methodInfo, final MethodVisitor methodVisitor) {
+    return new OBBNMethodVisitor(this, methodInfo, context, methodVisitor);
+  }
+
+  @Override
+  public String getGloballyUniqueId() {
+    return this.getClass().getName();
+  }
+
+  @Override
+  public String getName() {
+    return name();
+  }
+
+}
+
+class OBBNMethodVisitor extends AbstractInsnMutator {
+
+  OBBNMethodVisitor(final MethodMutatorFactory factory,
+                    final MethodInfo methodInfo, final MutationContext context,
+                    final MethodVisitor writer) {
+    super(factory, methodInfo, context, writer);
+  }
+
+  private static final Map<Integer, ZeroOperandMutation> MUTATIONS = new HashMap<Integer, ZeroOperandMutation>();
+
+  static {
+    //Ints
+    MUTATIONS.put(Opcodes.IOR, new InsnSubstitution(Opcodes.IAND,
+            "Replaced bitwise OR with AND (OBBN)"));
+    MUTATIONS.put(Opcodes.IAND, new InsnSubstitution(Opcodes.IOR,
+            "Replaced bitwise AND with OR (OBBN)"));
+
+    //Longs
+    MUTATIONS.put(Opcodes.LOR, new InsnSubstitution(Opcodes.LAND,
+            "Replaced bitwise OR with AND (OBBN)"));
+    MUTATIONS.put(Opcodes.LAND, new InsnSubstitution(Opcodes.LOR,
+            "Replaced bitwise AND with OR (OBBN)"));
+  }
+
+  @Override
+  protected Map<Integer, ZeroOperandMutation> getMutations() {
+    return MUTATIONS;
+  }
+
+}
